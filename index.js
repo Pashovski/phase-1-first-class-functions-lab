4ea290bf06bd800f039660294daab1ac3862a615
Pasha <pashdoctor@gmail.com>
2021-09-05 09:22:49 -0400

    Stuck on q 3

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 // Code your solution in this file!
 const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
 function returnFirstTwoDrivers(){
-
+    return drivers.slice(0, 2)
 }
 
-drivers.slice(0, 2)
+// drivers.slice(0, 2)
+
+function returnLastTwoDrivers(){
+    return drivers.slice(2,4)
+}
+
+const selectingDrivers = ['function returnFirstTwoDrivers', 'function returnLastTwoDrivers'];
